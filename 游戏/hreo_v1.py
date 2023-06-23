6a27d379cde645c7112f162ffeebbc32d3d8a673
Libre Gu <>
2023-06-23 09:49:55 +0800

    分增加一些代码

--- 游戏/hreo_v1.py
+++ 游戏/hreo_v1.py
@@ -7,10 +7,12 @@ FilePath: /PythonJupyter/hreo_v1.py
 Description: 
 Copyright (c) 2023 by Libre, All Rights Reserved. 
 '''
-# 初始化 BEGIN
+## 初始化 BEGIN
 # 游戏背景
 __GAME_NAME__ = "笑傲江湖"
 __GAME_VERSION__ = "1.0.0"
+# 分割线宽度
+__WIDTH__ = 30
 
 print("=====================")
 print("{0} v{1}".format(__GAME_NAME__, __GAME_VERSION__))
@@ -23,15 +25,17 @@ print("""
 ......
 """)
       
-# 创建主角
+# 定义字典键名
 name = "name"   # 名字键名
 hp = "hp"       # 血量键名       
 mp = "mp"       # 魔法值键名
 attack = "attack"   # 攻击力键名
 skills = "skills"   # 技能键名
+
+# 创建主角
 my = {
     name: "张无忌",
-    hp: 100,
+    hp: 2000,
     mp: 100,
     attack: 10,
     skills: {
@@ -57,9 +61,9 @@ my = {
 
 # NPC列表
 enemys = [
-    {name: "东方不败",hp: 0,attack: 10},
-    {name: "岳不群",hp: 0,attack: 20},
-    {name: "任我行",hp: 0,attack: 30}
+    {name: "东方不败",hp: 100,attack: 10},
+    {name: "岳不群",hp: 100,attack: 20},
+    {name: "任我行",hp: 100,attack: 30}
 ]
 
 # 测试代码
@@ -68,11 +72,37 @@ enemys = [
 #     print("{1}.{0[name]}: 生命:{0[hp]}, 攻击力:{0[attack]}".format(enemys[i], i))
 # print("----------------------------------")
 
+## 初始化 END
 
 # 开始游戏回合
 # 游戏结束的条件：我方生命0或者敌人生命之和为0
 while True:
-    # 每一个回合代码
+    ## 每一个回合代码
+
+    # 展示主角基本信息
+    print("{0:-^30}".format(my[name]+'信息'))
+    print("生命：{0}, 魔法：{1}".format(my[hp], my[mp]))
+    print("-" * __WIDTH__)
+
+    # 展示敌人信息
+    print("{:-^30}".format('敌人信息'))
+    for i in range(len(enemys)):
+        print("{1}.{0[name]}: 生命:{0[hp]}, 攻击力:{0[attack]}".format(enemys[i], i))
+    print("-" * __WIDTH__)
+
+    # 选择要攻击的敌人编号
+    enemy_num = 0 # 储存选择的敌人编号
+    enemy_num = int(input('请选择敌人编号：')) % len(enemys)
+    print(enemy_num)
+    
+
+    #TODO 展示技能
+
+    #TODO 选择技能
+
+    #TODO 用选择好的技能攻击选中的敌人
+
+    #TODO 敌人回击
 
     # 判断游戏结束
     enemy_hp_sum = 0
@@ -88,5 +118,3 @@ if my[hp] > 0 and enemy_hp_sum <= 0:
     print("主角胜利")
 else:
     print("敌人胜利")
-
-# 初始化 END
