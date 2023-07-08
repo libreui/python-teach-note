b349cc725e7784bdd282421a0529eb57b12eb048
Libre Gu <>
2023-07-09 07:53:06 +0800

    字典、武侠游戏v2

--- 游戏/hreo_v2.py
+++ 游戏/hreo_v2.py
@@ -1,18 +1,18 @@
-'''
-Author: Libre Gu 
-Date: 2023-06-03 08:36:49
-LastEditors: Libre Gu 
-LastEditTime: 2023-06-03 09:07:35
-FilePath: /PythonJupyter/hreo_v1.py
-Description: 
-Copyright (c) 2023 by Libre, All Rights Reserved. 
-'''
+import random
+
 ## 初始化 BEGIN
 # 游戏背景
 __GAME_NAME__ = "笑傲江湖"
-__GAME_VERSION__ = "1.0.0"
+__GAME_VERSION__ = "2.0.0"
 # 分割线宽度
 __WIDTH__ = 30
+
+# 最大MP
+__MAX_MP__ = 100
+# 最大HP
+__MAX_HP__ = 2000
+
+#TODO 升级的逻辑代码
 
 print("=====================")
 print("{0} v{1}".format(__GAME_NAME__, __GAME_VERSION__))
@@ -29,21 +29,29 @@ print("""
 name = "name"   # 名字键名
 hp = "hp"       # 血量键名       
 mp = "mp"       # 魔法值键名
+eva = "eva"     # 闪避值键名
 attack = "attack"   # 攻击力键名
 skills = "skills"   # 技能键名
 
 # 创建主角
 my = {
     name: "张无忌",
-    hp: 2000,
-    mp: 100,
-    attack: 10,
+    hp: __MAX_HP__,
+    eva: 50,
+    mp: __MAX_MP__,
     skills: {
+        "普通攻击": {'attack': 10, 'mp': 0},
         "华山剑法": {'attack': 200, 'mp': 10},
         "独孤九剑": {'attack': 20, 'mp': 20},
         "葵花宝典": {'attack': 30, 'mp': 30}
     }
 }
+
+all_skills = my[skills].keys()
+for i in range(len(all_skills)):
+   skill_name =  all_skills[i]
+   print(my[skills][skill_name])
+
 
 # 测试主角
 # print('---测试主角--------------------------')
@@ -79,6 +87,20 @@ enemys = [
 while True:
     ## 每一个回合代码
 
+    ### 每回合初始化操作 ###
+
+    # 回复生命
+    my[hp] += 10
+    if my[hp] > __MAX_HP__:
+        my[hp] = __MAX_HP__
+    
+    # 回复魔法值
+    my[mp] += 5
+    if my[mp] > __MAX_MP__:
+        my[mp] = __MAX_MP__
+
+    ### 每回合初始化操作 ###
+
     # 展示主角基本信息
     print("\n{0:-^30}".format(my[name]+'信息'))
     print("生命：{0}, 魔法：{1}".format(my[hp], my[mp]))
@@ -87,7 +109,11 @@ while True:
     # 展示敌人信息
     print("\n{:-^30}".format('敌人信息'))
     for i in range(len(enemys)):
-        print("{1}.{0[name]}: 生命:{0[hp]}, 攻击力:{0[attack]}".format(enemys[i], i))
+        enemy_hp = enemys[i][hp]
+        # 如果敌人血量小于0判断
+        if enemy_hp < 0:
+            enemy_hp = 0
+        print("{1}.[{0[name]}]: \n    生命:{2}, 攻击力:{0[attack]}".format(enemys[i], i, enemy_hp))
     print("-" * __WIDTH__)
 
     # 选择要攻击的敌人编号
@@ -100,17 +126,28 @@ while True:
     for snum in range(len(all_skills)):
         skill_key = all_skills[snum] # 技能字典的键
         skill_attack = my[skills][skill_key][attack] #技能攻击力
-        print("%s.%s 攻击力 %s" % (snum, skill_key, skill_attack))
+        skill_mp = my[skills][skill_key][mp] # 技能消耗的mp
+        print("%s.[%s] \n    攻击力 %s 消耗魔法 %s" % (snum, skill_key, skill_attack, skill_mp))
     print('-'*__WIDTH__)
 
     # 选择技能编号
     chice_snum = int(input("请选择技能：")) % len(all_skills)
     chice_skill = my[skills][all_skills[chice_snum]]
 
+    # 判断主角剩余魔法值是否够本次技能施展
+    if my[mp] < chice_skill[mp]:
+        print("\n{:*^30}".format(' 错误 '))
+        print("魔法值不足！")
+        print('*'*__WIDTH__)
+        continue
+
     # 用选择好的技能攻击选中的敌人
     attack_enemy = enemys[enemy_num] #被选择敌人的字典信息
     skill_attack = chice_skill[attack]
     attack_enemy[hp] -= skill_attack
+
+    # 技能消耗MP值
+    my[mp] -= chice_skill[mp]
 
     # 当前目标死亡标识
     dead = ""
@@ -124,11 +161,16 @@ while True:
         # 如果敌人死亡则不攻击
         if enemy[hp] <= 0:
             continue
-        # 殴打主角
-        my[hp] -= enemy[attack]
-        # 旁白
-        template = "\n「{0} 攻击了 {1}, -{2} 生命 {3}」"
-        print(template.format(enemy[name], my[name], enemy[attack], my[hp]))
+        # 回避率的判断
+        if random.randint(1, 100) < my[eva]:
+            print("\n「躲过了 {0} 的攻击」".format(enemy[name]))
+        else:
+            # 殴打主角
+            my[hp] -= enemy[attack]
+
+            # 旁白
+            template = "\n「{0} 攻击了 {1}, -{2} 生命 {3}」"
+            print(template.format(enemy[name], my[name], enemy[attack], my[hp]))
 
     # 判断游戏结束
     enemy_hp_sum = 0
